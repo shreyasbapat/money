5a6a85d45a6e76c9e3413c77a3a2912496e5d262
Shreyas Bapat <bapat.shreyas@gmail.com>
2022-01-28 01:34:23 +0530

    Make first commit

---
+++ money/moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyApp.swift
+//  money
+//
+//  Created by Shreyas Bapat on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
